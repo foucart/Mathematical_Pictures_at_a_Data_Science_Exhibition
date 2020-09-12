85ef62f63b2a026381b4163fc115235c8599d7a4
Simon Foucart <simon.foucart@gmail.com>
2020-09-12 18:44:50 -0500

    Add files via upload

--- Chapter07/Chapter07.m
+++ Chapter07/Chapter07.m
@@ -13,7 +13,7 @@ m = sum(M);                       % total number of datapoints
 X = [];                           % dxm matrix to hold the datapoints (d=3)
 for h = 1:k
     center_h = [cos(2*pi/k*h); sin(2*pi/k*h); .05*rand];  
-    radius_h = rand/h;
+    radius_h = rand/k;
     X_h = [center_h(1) + radius_h*randn(1,M(h));...
            center_h(2) + radius_h*randn(1,M(h));
            center_h(3) + .05*randn(1,M(h))];
@@ -61,6 +61,7 @@ for h=1:k
 end
 title('Clusters produced by single linkage')
 
+
 %% naive implementation of Lloyd's algorithm
 centers = [4*rand(2,k)-2; zeros(1,k)];   % dxk matrix to holds the centers (d=3)
 partition_l = zeros(m,k);                % its columns will hold the clusters
@@ -74,9 +75,11 @@ for t=1:n_iters
     end
     [~,aux] = min(dist);
     for h=1:k
-        C_h = find(aux==h);                          % the h-th cluster as a set
-        partition_l(:,h) = (aux==h);                 % the h-th cluster as a 0/1-vector
-        centers(:,h) = sum(X(:,C_h),2)/length(C_h);  % the h-th center
+        C_h = find(aux==h);                              % the h-th cluster as a set
+        partition_l(:,h) = (aux==h);                     % the h-th cluster as a 0/1-vector
+        if length(C_h) > 0
+            centers(:,h) = sum(X(:,C_h),2)/length(C_h);  % the h-th center
+        end
    end
 end
 % visualize the ouputted clusters
@@ -97,19 +100,20 @@ m = 500;
 perm = randperm(m);
 comm1 = sort(perm(1:m/2));
 comm2 = sort(perm(m/2+1:m));
-% define an auxiliary 0/1-matrix whose entries = 0 w.proba=p
+% define an auxiliary 0/1-matrix whose entries = 1 w.proba=p
 p = 1/4;
-aux2 = (rand(m/2,m/2)<p);
+aux1 = (rand(m/2,m/2)<p);
 % define an auxiliary 0/1-matrix whose entries = 1 w.proba=q
 q = 3/4;
-aux1 = (rand(m/2,m/2)<q);   
-aux1l = tril(aux1) - diag(diag(aux1));
-aux1u = triu(aux1) - diag(diag(aux1));
+aux2 = (rand(m/2,m/2)<q);   
+aux2l = tril(aux2) - diag(diag(aux2));
+aux2u = triu(aux2) - diag(diag(aux2));
 % define the random adjacency matrix W
-W(comm1,comm1) = aux1l+aux1l';
-W(comm1,comm2) = aux2;
-W(comm2,comm1) = aux2';
-W(comm2,comm2) = aux1u+aux1u';
+W = zeros(m,m);
+W(comm1,comm1) = aux2l+aux2l';
+W(comm1,comm2) = aux1;
+W(comm2,comm1) = aux1';
+W(comm2,comm2) = aux2u+aux2u';
 
 %% Cluster according to the leading eigenvector of W - delta*ones(n,n)
 delta=sum(sum(W))/m^2;
@@ -119,7 +123,7 @@ comm2_sp = find(V(:,1)<=0)';
 % determine the percentage of errors made
 n1 = length(intersect(comm1,comm1_sp))+length(intersect(comm2,comm2_sp));
 n2 = length(intersect(comm1,comm2_sp))+length(intersect(comm2,comm1_sp));
-disp(strcat('Percentage of errors:', 32, num2str(100*min(n1,n2)/100), 37))
+disp(strcat('Percentage of errors:', 32, num2str(100*min(n1,n2)/m), 37))
            
 %% There seems to be no errors at all when m is large enough!
 % Empirical estimation of the error percentage depending on m, p, and q
@@ -135,9 +139,9 @@ nb_trials = 50;
 q = 3/4;
 p_values = [1/10, 1/5, 1/4, 1/2];
 % matrix to store the percentage of errors
-nb_errors = zeros(1+ (m_max-m_min)/m_inc ,length(p_values));
+nb_errors = zeros( 1+(m_max-m_min)/m_inc ,length(p_values) );
 % loop reproducing the above code
-for j=1:4
+for j=1:length(p_values)
     for m = m_min:m_inc:m_max
         err = 0;
         for t=1:nb_trials            
@@ -171,7 +175,7 @@ plot(m_min:m_inc:m_max,100*nb_errors(:,1),'-*',...
     m_min:m_inc:m_max,100*nb_errors(:,3),'-+',...
     m_min:m_inc:m_max,100*nb_errors(:,4),'-s');
     title(strcat('q=',num2str(q)));
-    xlabel('size m of the randon graph');
+    xlabel('size m of the random graph');
     ylabel('percentage of vertices put in the wrong community');
 legend(strcat('p=',num2str(p_values(1))),...
     strcat('p=',num2str(p_values(2))),...
